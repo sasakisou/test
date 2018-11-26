0af4b262e152b51d3fe13084a9cd9f386a8ea8a3
sasakisou <sasaki.sou7531@gmail7531@gmail.com>
2018-11-26 15:37:44 +0900

    11/26/new

---
+++ Person/src/person/Robot.java
@@ -0,0 +1,15 @@
+package person;
+
+public class Robot {
+	public String name=null;
+	public void walk(){
+		System.out.println(this.name + "が歩く");
+	}
+	public void talk(){
+		System.out.println(this.name + "が話す");
+	}
+	public void run(){
+		System.out.println(this.name + "が走る");
+	}
+
+}
