27a48381bd7d5ba8eb86817128c37bd416f93059
sasakisou <sasaki.sou7531@gmail.com>
2018-12-04 17:56:44 +0900

    12/4/last

---
+++ WebProj/src/com/internousdev/webproj/action/InquiryCompleteAction.java
@@ -0,0 +1,12 @@
+package com.internousdev.webproj.action;
+
+import opensymphony.xwork2.ActionSupport;
+
+public class InquiryCompleteAction extends ActionSupport{
+	private String name;
+	private String qtype;
+	private String body;
+
+	public String
+
+}
